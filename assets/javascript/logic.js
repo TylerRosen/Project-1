cd5f8f9bab01ccb305f2ca279e21150a53ce46ce
TylerRosen <tylermrosen@gmail.com>
2017-01-06 21:27:37 -0500

    Updated HTMl and Javascript

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,22 +1,29 @@
-console.log("Hello")
-
-$("#play_song").on("click", function () {
-	console.log("Song!");
-
-var queryURL = ""
-        console.log(queryURL);
-
-          $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-        .done(function(response) {
-          var results = response.data;
-
-var song = "spotify:track:6rqhFgbbKwnb9MLmUQDhG6"
-
-song.play ();
-
+$("#submit").on("click", function () {
+    console.log("Song!");
+    var track = $('#user-input').val().trim();
+    var searchQuery = "";
+    var queryURL = 'https://api.spotify.com/v1/search?q=' + track + '&type=track&limit=1';
+       console.log(queryURL);
+         $.ajax({
+         url: queryURL,
+         method: "GET"
+       })
+       .done(function(response) {
+         var results = response;
+         console.log(results);
+         // console.log(results.tracks.items[0]);
+         console.log(results.tracks.items[0].name);
+         console.log(results.tracks.items[0].artists[0].name);
+         console.log(results.tracks.items[0].id);
+         var trackId = results.tracks.items[0].id;
+         var link = "https://open.spotify.com/embed/track/" + trackId;
+         console.log(link);
+$('#tracks').append(results.tracks.items[0].name);
+$('#tracks2').append($('<div>').addClass('song').append('<embed src=' + link +'>'));
+return false;
+// var song = "spotify:track:6rqhFgbbKwnb9MLmUQDhG6"
+// song.play();
+var song = "spotify:track:" + songID 
+var songId= results.tracks.items[0].id;
 });
-
 });
