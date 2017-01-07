3347a76942d63e03a2198d6449d4dd46024d4279
TylerRosen <tylermrosen@gmail.com>
2017-01-06 23:06:53 -0500

    Added timer, need to convert

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,14 @@
+//Global Variables
+
+var voteDiv = $("<div id='vote'>");
+	var vote1 = $('<input type = "button" value="Vote"/>');
+	var vote2 = $('<input type = "button" value="Vote"/>')
+
+	var counter= 181
+	var timer;
+
+//---------------------------------------------------------------
+
 $("#submit").on("click", function () {
     console.log("Song!");
     var track = $('#user-input').val().trim();
@@ -29,12 +40,56 @@ var songId= results.tracks.items[0].id;
 });
 
 $("#battle").on("click", function () {
-	$("#user-input, #submit, #battle").hide();
 
-	var voteDiv = $("<div id='vote'>");
-	var vote1 = $('<input type = "button" value="Vote"/>');
-	var vote2 = $('<input type = "button" value="Vote"/>')
+//Tells timer to decrease every second
+function run () {
+ timer = setInterval(decrement, 1000);
+}
+
+
+	//Counts down seconds
+function decrement () {
+	counter--;
+
+//Converts miliseconds into minutes and seconds
+	var converted = timeConverter(counter);
+	$("#timer").html(counter + converted);
+
+//Once timer reaches 0, stop the timer
+	if (counter === 0) {
+
+	stop();
+
+  };
+
+};
+
+run();
+
+
+//Converts seconds into minutes
+function timeConverter(t) {
+
+  var minutes = Math.floor(t / 60);
+  var seconds = t - (minutes * 60);
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  if (minutes === 0) {
+    minutes = "00";
+  }
+  else if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  return minutes + ":" + seconds;
+}
+
+	$("#user-input, #submit, #battle").hide();
 
 	$("#searchbar").prepend(voteDiv).html("Vote Now!");
 	$("#vote").prepend(vote1, vote2);
+
 });
